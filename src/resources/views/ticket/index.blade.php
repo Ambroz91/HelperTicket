fd2a1ceefcd077ef3ab534f21c698cf5af339d8d
Ambroz <p.ambroz91@gmail.com>
2023-05-25 17:39:06 +0200

    [FEATURE] Store Reply
    Some adjustments to the routes allowing to pass the ticket_id/ticket_slug in it.
    Removed the old home view

--- src/resources/views/ticket/index.blade.php
+++ src/resources/views/ticket/index.blade.php
@@ -5,10 +5,11 @@
 
         @foreach($tickets as $ticket)
             <div class="my-2 bg-gray-800 text-white p-3">
-                <a href="{{ route('ticket.show',[$ticket->slug]) }}"><h1
+                <a href="{{ route('ticket.show', $ticket->slug) }}"><h1
                         class="text-2xl">{{\Illuminate\Support\Str::of($ticket->title)->upper()}}</h1></a>
                 <div>{{\Illuminate\Support\Str::limit($ticket->description,150)}}</div>
                 <div>{{$ticket->created_at->diffForHumans()}}</div>
+                <div>{{strtoupper($ticket->status)}}</div>
                 @if($ticket->replies != null)
                     <div>Replies: {{$ticket->replies->count }}</div>
                 @else
